c590b1ea0e99200c672795474857f64c8b7bf8b5
Sci-geek <Joel@dhcp-10-132-133-139.uawifi.arizona.edu>
2017-11-10 16:37:11 -0700

    added Event

--- src/org/system/event/Event.java
+++ src/org/system/event/Event.java
@@ -1,5 +1,58 @@
 package org.system.event;
 
 public class Event {
+	private String eventName;
+	private Time eventTime;
+	private Venue eventVenue;
+	private String description;
+	
+	
+	public Event() {
+		this.setEventName("null");
+		this.setEventTime(null);
+		this.setEventVenue(null);
+	}
+	
+	public Event(String eventName, Venue eventVenue, Time eventTime) {
+		this.setEventName(eventName);
+		this.setEventTime(eventTime);
+		this.setEventVenue(eventVenue);
+	}
+
+
+	public Time getEventTime() {
+		return eventTime;
+	}
+
+	public void setEventTime(Time eventTime) {
+		this.eventTime = eventTime;
+	}
+
+	public Venue getEventVenue() {
+		return eventVenue;
+	}
+
+	public void setEventVenue(Venue eventVenue) {
+		this.eventVenue = eventVenue;
+	}
+
+	public String getEventName() {
+		return eventName;
+	}
+
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 
 }
+
+

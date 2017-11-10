7248f0ad66bdea4b040e6673f51ebf301a0165b3
Arifin Grace <agrace1@email.arizona.edu>
2017-11-10 15:06:04 -0700

    created Customer class

--- src/org/system/customer/Customer.java
+++ src/org/system/customer/Customer.java
@@ -1,5 +1,77 @@
 package org.system.customer;
 
+import java.util.*;
+
+import org.system.event.*;
+
 public class Customer {
 
+	​private String name; 
+	​private Address address;​
+	private String phone;
+	private ​String email;
+	private ​PaymentInfo paymentCard;
+	​private ArrayList<Seat> seatsPurchased;​
+
+	
+	private Customer() {
+		this.name = "Unknown";
+		this.address = new Address();
+		this.phone = "0000000000";
+		this.email = "none@none.com";
+		this.paymentCard = new PaymentInfo();
+		this.seatsPurchased = new ArrayList<Seat>();
+	}
+
+	//Getters
+
+	public String getName() {
+		return this.name;
+	}
+	
+	public Address getAddress() {
+		return this.address;
+	}
+
+	public String getPhone() {
+		return this.phone;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public PaymentInfo getPaymentCard() {
+		return this.paymentCard;
+	}
+
+	public ArrayList<Seat> getSeatsPurchased() {
+		return this.seatsPurchased;
+	}
+	
+	//Setters
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPaymentCard(PaymentInfo paymentCard) {
+		this.paymentCard = paymentCard;
+	}
+
+	public void setSeatsPurchased(ArrayList<Seat> seatsPurchased) {
+		this.seatsPurchased = seatsPurchased;
+	}
+	
 }
